89bc3a9dd8d02ef5e9c9b0c6d7feecd9f89967d7
sathyaeraj <sathyagovindaraj@gmail.com>
2019-06-25 17:11:16 -0400

    American guy commit

--- src/test/java/qaclickacademy/BlankTest.java
+++ src/test/java/qaclickacademy/BlankTest.java
@@ -10,6 +10,12 @@ public class BlankTest {
 		System.out.println("Hello I am optional");
 		System.out.println("Hello testing the updation to Github");
 		System.out.println("Pushing the updates to Github");
+		
+		
+		System.out.println("Messge1");
+		System.out.println("Message2");
+		System.out.println("PMesssage3");
+		
 	}
 
 }
