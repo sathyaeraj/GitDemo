e7a76eb8b59445a53b2870d2d8e60ad565740ac7
sathyaeraj <sathyagovindaraj@gmail.com>
2019-06-25 11:18:28 -0400

    first commit

---
+++ src/test/java/qaclickacademy/BlankTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class BlankTest {
+	
+	@Test
+	public void  hello()
+	{
+		System.out.println("Hello I am optional");
+	}
+
+}
